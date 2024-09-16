ae7e2cc318d6cdda1770a939e7d4bc7d9ec93da1
João Mantovani <148283083+JoaoMantovani1104@users.noreply.github.com>
2024-09-16 09:37:06 -0300

    Delete JavaScript directory

--- JavaScript/src/verificarRespostas.js
+++
@@ -1,6 +0,0 @@
-function verificar(alternativasGabarito, alternativasResposta) {
-    return alternativasResposta.length <= alternativasGabarito.length &&
-        alternativasResposta.every(resposta => alternativasGabarito.some(alt => alt.includes(resposta)));
-}
-
-module.exports = verificar;
